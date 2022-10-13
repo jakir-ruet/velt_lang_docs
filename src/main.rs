3d48536f1fec097d39642002b2169f11133d0722
tm-ahad <tm.ahad.07@gmail.com>
2022-10-13 22:19:16 +0600

    sidebar added

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use actix_web::{App, HttpServer};
 use actix_files::Files;
-
-#[get("/")]
-async fn greet() -> impl Responder {
-    HttpResponse::Ok().body(include_str!("../views/index.html"))
-}
-
-#[get("/install")]
-async fn install() -> impl Responder {
-    HttpResponse::Ok().body(include_str!("../views/quick_start.html"))
-}
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(Files::new("/static", "../../velt/target/debug"))
-            .service(greet)
-            .service(install)
+            .service(Files::new("/v1", "../download"))
+            .service(Files::new("/web", "../views"))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
