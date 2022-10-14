d363475fe86e94b242b6a0ec8acf56cc9c6d2116
tm-ahad <tm.ahad.07@gmail.com>
2022-10-14 11:13:23 +0600

    project updated.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web, get, Responder, HttpResponse};
 use actix_files::Files;
 
+#[get("/docs/{n}")]
+async fn docs(n: web::Path<String>) -> impl Responder
+{
+    let name = n.into_inner();
+
+    if name == "hello_world.html"
+    {
+        return HttpResponse::Ok().body(format!("{}{}", include_str!("../views/hello_world.html"), include_str!("../views/docs.html")))
+    }
+    return if name == "variables_&_specifier.html"
+    {
+        HttpResponse::Ok().body(format!("{}{}", include_str!("../views/variables_&_specifier.html"), include_str!("../views/docs.html")))
+    } else {
+        HttpResponse::Ok().body(include_str!("../views/docs.html"))
+    }
+
+}
+
 #[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()>
+{
     HttpServer::new(|| {
         App::new()
+            .service(docs)
             .service(Files::new("/v1", "../download"))
             .service(Files::new("/web", "../views"))
     })
