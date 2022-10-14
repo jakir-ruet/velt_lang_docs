ffcaabed16a0fd232e8bde52913d42c7161d9cce
tm-ahad <tm.ahad.07@gmail.com>
2022-10-14 17:26:11 +0600

    Getting started completed.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,44 @@
-use actix_web::{App, HttpServer, web, get, Responder, HttpResponse};
 use actix_files::Files;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/docs/{n}")]
-async fn docs(n: web::Path<String>) -> impl Responder
-{
+async fn docs(n: web::Path<String>) -> impl Responder {
     let name = n.into_inner();
 
-    if name == "hello_world.html"
-    {
-        return HttpResponse::Ok().body(format!("{}{}", include_str!("../views/hello_world.html"), include_str!("../views/docs.html")))
+    if name == "hello_world.html" {
+        return HttpResponse::Ok().body(format!(
+            "{}{}",
+            include_str!("../views/hello_world.html"),
+            include_str!("../views/docs.html")
+        ));
     }
-    return if name == "variables_&_specifier.html"
-    {
-        HttpResponse::Ok().body(format!("{}{}", include_str!("../views/variables_&_specifier.html"), include_str!("../views/docs.html")))
+    if name == "variables_&_specifier.html" {
+        return HttpResponse::Ok().body(format!(
+            "{}{}",
+            include_str!("../views/variables_&_specifier.html"),
+            include_str!("../views/docs.html")
+        ));
+    }
+    if name == "getting_started.html" {
+        return HttpResponse::Ok().body(format!(
+            "{}{}",
+            include_str!("../views/getting_started.html"),
+            include_str!("../views/docs.html")
+        ));
     } else {
         HttpResponse::Ok().body(include_str!("../views/docs.html"))
     }
-
 }
 
 #[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()>
-{
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(docs)
             .service(Files::new("/v1", "../download"))
             .service(Files::new("/web", "../views"))
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
